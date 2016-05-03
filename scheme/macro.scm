81a24907c6113befaaa523c0dbb12e1bbb6388a2
Christian Jaeger <ch@christianjaeger.ch>
2016-05-03 22:25:28 +0200

    macro example with quasiquote

--- scheme/macro.scm
+++ scheme/macro.scm
@@ -3,17 +3,12 @@
   (mcase params
 	 (`(`var `start `end)
 	  (let ((LP 'f)) ;; 
-	    (list 'letrec
-		  (list
-		   (list
-		    LP
-		    (list 'lambda
-			  (list var)
-			  (list 'if
-				(list '< var end)
-				(cons 'begin
-				      (append body
-					      (list (list LP
-							  (list 'inc var)))))))))
-		  (list LP start))))))
+	    `(letrec
+		 ((,LP
+		   (lambda (,var)
+		     (if (< ,var ,end)
+			 (begin
+			   ,@body
+			   (,LP (inc ,var)))))))
+	       (,LP ,start))))))
 
